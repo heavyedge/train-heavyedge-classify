a2fb7575a0a34f2a8a53ddfb3cb8240d1b1cd60e
Jisoo Song <jeesoo9595@snu.ac.kr>
2026-03-20 02:08:01 +0900

    maint: bump to v1.0.0

--- upload.py
+++ upload.py
@@ -4,8 +4,8 @@ from huggingface_hub import HfApi
 
 api = HfApi(token=os.getenv("HUGGINGFACE_TOKEN"))
 
-REPO = "jeesoo9595/heavyedge-classify-v0"
-MODEL_VERSION = "v0.0.4"
+REPO = "jeesoo9595/heavyedge-classify-v1"
+MODEL_VERSION = "v1.0.0"
 
 api.create_repo(
     repo_id=REPO,
